c2f427b2b0fc96b9587cd7db6ff3de406503e20a
adiwillhit <aditya2ma72@gmail.com>
2026-01-23 14:33:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import "./app.scss"
+import Dock from "./components/Dock"
+import Nav from "./components/Nav"
+function App() {
+  
+
+  return (
+   <main>
+    <Nav></Nav>
+    <Dock></Dock>
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import "./nav.scss"
+const Nav = () => {
+  return (
+    <nav>
+        <div className="left">
+
+            <div className="apple-icon">
+                <img src="./navbar-icons/apple.svg" alt="" />
+            </div>
+
+            <div className="nav-item">
+                <p>Aditya Rai</p>
+            </div>
+            <div className="nav-item">
+                <p>File</p>
+            </div>
+            <div className="nav-item">
+                <p>Window</p>
+            </div>
+            <div className="nav-item">
+                <p>Terminal</p>
+            </div>
+        </div>
+        <div className="right"></div>
+    </nav>
+  )
+}
+
+export default Nav
